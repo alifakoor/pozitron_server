e5d3569d92949862d72c8156fa52939dbc37c755
Ali Fakoor <ali.fakoor13@gmail.com>
2021-11-10 22:22:53 +0330

    delete client folder

--- client/src/services/auth.header.js
+++
@@ -1,9 +0,0 @@
-export default function authHeader () {
-    const user = JSON.parse(sessionStorage.getItem('user'))
-
-    if (user && user.token) {
-        return { 'x-access-token': user.token }
-    } else {
-        return {}
-    }
-}

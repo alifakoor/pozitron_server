68f8b2e9ce88975975d24004cefa9378230edc17
Ali Fakoor <ali.fakoor13@gmail.com>
2021-12-08 10:24:15 +0330

    fix bug in token

--- app/controllers/product.controllers.js
+++ app/controllers/product.controllers.js
@@ -5,56 +5,72 @@ const db = require('../db')
 
 // functions
 function getAll(req, res) {
-    db.product
-        .findAll({
+    db.business
+        .findOne({
             where: {
-                type: {
-                    [db.Op.in]: ['simple', 'variable']
-                },
-                businessId: req.businessId
-            },
-            include: [
-                {
-                    model: db.product,
-                    as: 'variations',
-                    include: [{ model: db.productmeta, as: 'meta' }]
-                },
-                {
-                    model: db.productmeta,
-                    as: 'meta'
-                },
-                {
-                    model: db.productImage,
-                    as: 'images',
-                    required: false
-                }
-            ],
-            order: [
-                [
-                    { model: db.product, as: 'variations' },
-                    'id',
-                    'ASC'
-                ]
-            ]
-        })
-        .then(products => {
-            if (products.length) {
-                res.status(200).json({
-                    success: true,
-                    message: 'The list of products found successfully.',
-                    data: products
-                })
-            } else {
-                res.status(200).json({
-                    success: false,
-                    message: 'There is not any products.'
-                })
+                userId: req.user.id
             }
+        })
+        .then(business => {
+            if (business) {
+                db.product
+                    .findAll({
+                        where: {
+                            type: {
+                                [db.Op.in]: ['simple', 'variable']
+                            },
+                            businessId: business.id
+                        },
+                        include: [
+                            {
+                                model: db.product,
+                                as: 'variations',
+                                include: [{ model: db.productmeta, as: 'meta' }]
+                            },
+                            {
+                                model: db.productmeta,
+                                as: 'meta'
+                            },
+                            {
+                                model: db.productImage,
+                                as: 'images',
+                                required: false
+                            }
+                        ],
+                        order: [
+                            [
+                                { model: db.product, as: 'variations' },
+                                'id',
+                                'ASC'
+                            ]
+                        ]
+                    })
+                    .then(products => {
+                        if (products.length) {
+                            res.status(200).json({
+                                success: true,
+                                message: 'The list of products found successfully.',
+                                data: products
+                            })
+                        } else {
+                            res.status(200).json({
+                                success: false,
+                                message: 'There is not any products.'
+                            })
+                        }
 
+                    })
+                    .catch(err => {
+                        console.log(err)
+                        res.status(400).send({ success: false, message: 'something wrong is happened in list of products.' })
+                    })
+            } else {
+                res.status(200).send({ success: false, message: 'user business not found.' })
+            }
         })
         .catch(err => {
             console.log(err)
-            res.status(400).send({ success: false, message: 'something wrong is happened in list of products.' })
+            res.status(500).send({ success: false, message: 'user business not found.' })
         })
 }
 

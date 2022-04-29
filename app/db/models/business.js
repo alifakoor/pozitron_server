59e98aadeb7526d92f6543cd10e4e57010be69b8
mohsen <mohzia1380@gmail.com>
2022-04-29 15:49:37 +0430

    correct validation business in model

--- app/db/models/business.js
+++ app/db/models/business.js
@@ -6,29 +6,29 @@ const sequelize = require('../conn');
 const Business = sequelize.define('business', {
 	domain: {
 		type: DataTypes.STRING(200),
-		allowNull: true
+		allowNull: true,
 		// unique: true,
-		// validate: {
-		// 	// is: /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]\.[a-zA-Z]{2,}$/i
-		// 	isUrl: true
-		// }
+		validate: {
+			// is: /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]\.[a-zA-Z]{2,}$/i
+			isUrl: true
+		}
 	},
 	title: DataTypes.STRING(150),
 	description:  DataTypes.TEXT,
 	onlineBusiness: DataTypes.BOOLEAN,
 	key: {
 		type: DataTypes.STRING(50),
-		allowNull: true
-		// validate: {
-		// 	is: /^(ck_)(.+)/i
-		// }
+		allowNull: true,
+		validate: {
+			is: /^(ck_)(.+)/i
+		}
 	},
 	secret: {
 		type: DataTypes.STRING(50),
-		allowNull: true
-		// validate: {
-		// 	is: /^(cs_)(.+)/i
-		// }
+		allowNull: true,
+		validate: {
+			is: /^(cs_)(.+)/i
+		}
 	},
 	status: DataTypes.BOOLEAN
 }, {
